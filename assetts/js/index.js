3114099e721c1ca75ca56cb24ba97841c1aaa31b
framirezj <12649259+framirezj@users.noreply.github.com>
2025-02-11 20:59:31 -0300

    created: interactividad para + y -

--- assetts/js/index.js
+++ assetts/js/index.js
@@ -3,4 +3,8 @@ precio = 400000
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
 
+/* variables */
+cantidad = document.querySelector('#cantidad');
+contador = Number(cantidad.textContent)
+
 
